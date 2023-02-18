4e803aa6dce833a1cf87f229ca9e654895a75c7c
itsmepayback <91263405+itsmepayback@users.noreply.github.com>
2023-02-17 22:20:54 -0500

    Update winapp.cpp

--- winapp/winapp.cpp
+++ winapp/winapp.cpp
@@ -1,18 +1,16 @@
-#include  <windows.h>
+#include <windows.h>
 #include <stdio.h>
-#include <accctrl.h>
-#include <aclapi.h>
 
-#define _WIN32_DCOM
-#include <comdef.h>
-#include <Wbemidl.h>
-#include <oleauto.h>
+
+https://github.com/JustasMasiulis/lazy_importer
 #include "lazy_importer.hpp"
+https://github.com/kokke/tiny-AES-c
 #include "aes.hpp"
+//the header that contains our exe file to encrypt.
 #include "he.h"
 
 
-#pragma function(memcmp, memcpy, memset)
+#pragma function(memcpy, memset)
 
 
 
@@ -23,7 +21,7 @@
 
 
 
-
+//custom crt functions implementation
 
 void* memset(void* DestInit, int Source, size_t Size)
 {
@@ -43,20 +41,24 @@ void* memcpy2(void* DestInit, void const* SourceInit, size_t Size)
 }
 
 int main() {
-	MessageBoxA(0, "lol", "lol", 0);
+	
 	SIZE_T shellcodeSize = sizeof(rawData);
-
+// the key to encrypt your exe file with. Keep in mind that if you want to keep the default initialization vector (iv), the key MUST be 32 characters long. Make sure that both variables are the same as these ones in the loader.
 	unsigned char key[] = "qwertyuiopasdfghjklzxcvbnmqwerty";
+	// default initialization vector used for this project. you may change it if you want to.
 	unsigned char iv[] = "\x9d\x02\x35\x3b\xa3\x4b\xec\x26\x13\x88\x58\x51\x11\x47\xa5\x98";
 
 	struct AES_ctx ctx;
 	AES_init_ctx_iv(&ctx, key, iv);
 	AES_CBC_encrypt_buffer(&ctx, rawData, shellcodeSize);
-
-	HANDLE file =CreateFileA("final.exe", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+// create our output file for the encrypted data.
+	HANDLE file =CreateFileA("final.bin", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	DWORD written = 0;
+	// write the encrypted data to it
 	WriteFile(file, rawData, sizeof(rawData), &written, NULL);
+	// close the handle.
 	CloseHandle(file);
+	MessageBoxA(0, "Your Exe File Has Been Encrypted Successfully.", "Success", 0);
 	ExitProcess(0);
 	
 }
